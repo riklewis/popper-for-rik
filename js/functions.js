331b997239719faf927b5bdd12b7c21f57212384
Rik Lewis <rik@rik.onl>
2018-02-15 12:09:56 +0000

    Updated menu items after migration and also added CSP meta tag to upgrade insecure requests

--- js/functions.js
+++ js/functions.js
@@ -148,10 +148,10 @@
 		}
     li.find("a").prepend('<i class="'+fa+' '+iconName+'"></i><br>');
 	}
-	addMenuIcon("menu-item-108","fa-info"); //about
-	addMenuIcon("menu-item-118","fa-lightbulb"); //projects
-	addMenuIcon("menu-item-109","fa-comments"); //contact
-	addMenuIcon("menu-item-110","fa-alarm-clock"); //now
+	addMenuIcon("menu-item-131","fa-info"); //about
+	addMenuIcon("menu-item-134","fa-lightbulb"); //projects
+	addMenuIcon("menu-item-132","fa-comments"); //contact
+	addMenuIcon("menu-item-133","fa-alarm-clock"); //now
 
 	// Open social share links in new windows
 	$(".share-post").on("click","a",function() {
